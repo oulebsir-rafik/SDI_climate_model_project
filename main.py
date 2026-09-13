4b3be62b28c520094d1743786580d99a5c82431b
oulebsir rafik <oulebsir.rafik.enp@gmail.com>
2026-09-13 22:44:03 +0100

    [CHORE] updated project deps and config

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ def main():
     print("Hello from desalination-modeling-project!")
 
 
-if __name__ == "__main__":
-    main()
+# from src.corso_watershed import delineate_corso_watershed
+
+# report = delineate_corso_watershed(
+#     outlet_lat=36.76588539444612,
+#     outlet_lon=3.454718910871684,
+#     output_dir="output/corso_watershed",
+# )
